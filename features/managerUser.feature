b81466704f4d1c0a425827ebff51103ff8c94566
Renato - Macbook <renatojoa@gmail.co>
2019-07-07 19:00:18 -0300

    weather list by id.

--- features/managerUser.feature
+++ features/managerUser.feature
@@ -3,13 +3,13 @@ Feature: Adidas Challenge API
 
  Scenario Outline: Check weather in <city>
   Given I submit request on url using <city> successfully
-  When I check status code: 200
-  Then I check temperature: <temp>
-  Then I check the min/max temperature: <minTemp> / <maxTemp>
-  Then I check pressure: <pressure>
-  Then I check humidity: <humidity>
-  Then I check Weather: <weather> and <descWeather>
+#   When I check status code: 200
+#   Then I check temperature: <temp>
+#   Then I check the min/max temperature: <minTemp> / <maxTemp>
+#   Then I check pressure: <pressure>
+#   Then I check humidity: <humidity>
+  Then I check weather by id: <weatherID>
 
    Examples:
-    | city     | temp  | minTemp | maxTemp | pressure | humidity | weather | descWeather      |
-    | London   | 19.97 | 17.78  |  21.67   | 1017     | 56       | Clouds  | overcast clouds  |
+    | city     | temp  | minTemp | maxTemp | pressure | humidity | weatherID |   |
+    | London   | 16.1  | 13.89   | 18.33   | 1018     | 63       | 520       |   |
--- features/steps/managerUser.py
+++ features/steps/managerUser.py
@@ -1,15 +1,15 @@
 import json
 import requests
-from jsonschema import validate
 from behave import given, when, then, step
 from behave import *
 import time
+from features.utils.util import UtilMethods
 
 use_step_matcher("re")
 #REQUESTS
 @given('I submit request on url using (?P<city>.+) successfully')
 def step_impl(context, city):
-    url = context.config.userdata['base_url'] + city + '&APPID=' + context.config.userdata['app_id'] + context.config.userdata['unitC']
+    url = context.config.userdata['base_url'] + city + '&appid=' + context.config.userdata['app_id'] + context.config.userdata['unitC']
     print(url)
     context.response = requests.get(url).json()
     assert context.response['cod'] == 200
@@ -25,8 +25,17 @@ def step_impl(context):
 #MAIN
 @Then('I check temperature: (?P<temp>.+)')
 def step_impl(context, temp):
-    print(float(context.response['main']['temp']))
-    assert float(context.response['main']['temp']) == float(temp)
+    print('temp normal: ')
+    print(float(temp))
+
+    print('temp +5: ')
+    print(float(temp) +0.5)
+
+    print('temp -5: ')
+    print(float(temp) -0.5)
+    print(context.response['main']['temp'])
+    print('oi')
+    assert (float(temp)-0.5) <= context.response['main']['temp'] <= (float(temp)+0.5)
 
 @Then('I check the min/max temperature: (?P<minTemp>.+) / (?P<maxTemp>.+)')
 def step_impl(context, minTemp, maxTemp):
@@ -37,18 +46,23 @@ def step_impl(context, minTemp, maxTemp):
 
 @Then('I check pressure: (?P<pressure>.+)')
 def step_impl(context, pressure):
-    assert context.response['main']['pressure'] == pressure
+    print(context.response['main']['pressure'])
+    assert context.response['main']['pressure'] == float(pressure)
 
 @Then('I check humidity: (?P<humidity>.+)')
 def step_impl(context, humidity):
-    assert context.response['main']['humidity'] == humidity
+    assert context.response['main']['humidity'] == float(humidity)
 #END MAIN
 
 #WEATHER
-@Then('I check Weather: (?P<weather>.+) and (?P<descWeather>.+)')
-def step_impl(context, weather, descWeather):
-    print(context.response['weather']['main'])
-    print(context.response['weather'][0]['description'])
+@Then('I check weather by id: (?P<weatherID>.+)')
+def step_impl(context, weatherID):
+    print(context.response['weather'][0]['main'])
+    result = UtilMethods.weatherIDContent(weatherID)
+    print(result[0])
+    print(result[1])
+    assert context.response['weather'][0]['main'] == result[0]
+    assert context.response['weather'][0]['description'] == result[1]
     # assert context.response['weather'][0]['main'] == weather
     # assert context.response['weather'][0]['description'] == descWeather
 #END WEATHER
